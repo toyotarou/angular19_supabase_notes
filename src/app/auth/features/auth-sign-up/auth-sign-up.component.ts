ec68b8eed0763c95243d1f06e6729910659d39a0
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-05-22 17:12:47 +0900

    commit 20250522 17:12

--- src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
+++ src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface SignUpForm {
+  email: FormControl<null | string>;
+  password: FormControl<null | string>;
+}
 
 @Component({
   selector: 'app-auth-sign-up',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: 'auth-sign-up.html',
   styleUrl: './auth-sign-up.component.css'
 })
 export default class AuthSignUpComponent {
+  private _formBuilder = inject(FormBuilder);
 
+  form = this._formBuilder.group<SignUpForm>({
+    email: this._formBuilder.control(null, [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control(null, [Validators.required]),
+  });
+
+  submit(){
+    console.log(this.form.value);
+  }
 }
